270faf8ad0124fe5b83df62c946bf0d1846e8f5c
carloshoyos98 <choyos@cifpfbmoll.eu>
2020-03-15 17:31:56 +0100

    feat: Añadir página

--- Buscador/buscador.js
+++ Buscador/buscador.js
@@ -12,6 +12,17 @@ function getTags() {
     return tags
 }
 
+//funcion que obtiene las etiquetas para añadirlas en la tabla
+function tableTags(tags) {
+    var tags = tags.split(" ");
+    var table_tags = ""
+    $.each(tags, 
+        function(index, tag) {
+            table_tags += "<div> class='badge badge-light'>" + tag + "</div>"
+        });
+}
+
+//funcion que obtiene el valor de tagmode para la url
 function getTagOption() {
     var option = $('.form-control.form-control-lg').change().val();
     if (option == 1) {
@@ -21,4 +32,31 @@ function getTagOption() {
     }
 }
 
+function getURL() {
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=";
+    url += getTags();
+    url += "&tagmode=" + getTagOption();
+    return url;
+}
 
+
+//funcion que crea la tabla en cada búsqueda
+function addTable(json) {
+    $("#taulaItems").html("");
+    $.each(json.items,
+        function(index, item) {
+            var element = $("<tr><td>" + item.title + "</td>"
+            + "<td><img src='" + item.media.m + "'></td>"
+            + "<td>" + item.date_taken + "</td>"
+            + "<td>" + item.author + "</td>"
+            + "<td>" + tableTags(item.tags) + "</td>" 
+            + "<td><a href='" + item.link + "' target='blank'>"
+            + "<button type='button' class='btn-warning'>Link</button></td></tr>");
+            element.appendTo("#taulaItems");
+        });
+}
+
+function getTable() {
+    var url = getURL();
+    $.getJSON(url, addTable);
+}
